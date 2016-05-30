7dd8d19f2b8fb61cbc66ac0485228b54fb886fa2
Adriel P. de Guzman <adeguzman@stratpoint.com>
2016-05-30 17:44:50 +0800

    Changed OAuth2 implementation, and added configurations package

--- src/main/java/io/adrieldg/Application.java
+++ src/main/java/io/adrieldg/Application.java
@@ -2,33 +2,17 @@ package io.adrieldg;
 
 import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 import io.adrieldg.entities.Journal;
 import io.adrieldg.entities.User;
 import io.adrieldg.repositories.JournalRepository;
-import io.adrieldg.repositories.UserRepository;
 import io.adrieldg.services.UserService;
 
 @SpringBootApplication
-@EnableAuthorizationServer
 public class Application {
 
 	@Bean
@@ -42,50 +26,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-}
-
-@Configuration
-class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
-
-	@Autowired
-	UserRepository userRepository;
-
-	@Override
-	public void init(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
-	}
-
-	@Bean
-	UserDetailsService userDetailsService() {
-		return new UserDetailsService() {
-
-			@Override
-			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-				User user = userRepository.findByUsername(username);
-				if (user != null) {
-					return new org.springframework.security.core.userdetails.User(user.getUsername(),
-							user.getPassword(), true, true, true, true, AuthorityUtils.createAuthorityList("USER"));
-				} else {
-					throw new UsernameNotFoundException("could not find the user '" + username + "'");
-				}
-			}
-
-		};
-	}
-
-	@Bean(name = "passwordEncoder")
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-}
-
-@EnableWebSecurity
-@Configuration
-class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic().and().csrf().disable();
-	}
-
 }
