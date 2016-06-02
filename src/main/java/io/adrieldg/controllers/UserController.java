227cf0bb08aaea0a6ad55cc5a256d4092323cb6f
Adriel P. de Guzman <adeguzman@stratpoint.com>
2016-06-02 10:28:23 +0800

    Changed return type of .save() to ResponseEntity

--- src/main/java/io/adrieldg/controllers/UserController.java
+++ src/main/java/io/adrieldg/controllers/UserController.java
@@ -1,6 +1,8 @@
 package io.adrieldg.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +15,7 @@ public class UserController {
   private UserService userService;
 
   // @RequestMapping(path = "/users", method = RequestMethod.POST)
-  User save(@RequestBody User user) {
-    return userService.registerUser(user);
+  ResponseEntity<User> save(@RequestBody User user) {
+    return new ResponseEntity<User>(userService.registerUser(user), HttpStatus.CREATED);
   }
 }
