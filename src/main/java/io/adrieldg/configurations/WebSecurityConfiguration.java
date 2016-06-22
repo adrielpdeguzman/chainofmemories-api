28f023b465f13006a983206bb32875450f5cc6f6
Adriel P. de Guzman <adeguzman@stratpoint.com>
2016-06-22 17:36:07 +0800

    Successfully enabled CORS, and added logging level to properties

--- src/main/java/io/adrieldg/configurations/WebSecurityConfiguration.java
+++ src/main/java/io/adrieldg/configurations/WebSecurityConfiguration.java
@@ -1,22 +1,37 @@
 package io.adrieldg.configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import io.adrieldg.services.CustomUserDetailsService;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+  @Value("${global.clientUrl}")
+  private String clientUrl;
+
   @Autowired
   CustomUserDetailsService userDetailsService;
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+  }
 
   @Override
   public void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -40,5 +55,19 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     http.authorizeRequests()
         .anyRequest().authenticated();
     /*@formatter:on*/
+  }
+
+  @Bean
+  public FilterRegistrationBean corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin(this.clientUrl);
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    source.registerCorsConfiguration("/**", config);
+    FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+    bean.setOrder(0);
+    return bean;
   }
 }
