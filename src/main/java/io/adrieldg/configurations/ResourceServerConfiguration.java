fa49a5b6561be2c4acbeb60fb26ad03ea1fa2353
Adriel P. de Guzman <adeguzman@stratpoint.com>
2016-07-01 15:34:52 +0800

    Fixed proper location of @EnableGlobalMethodSecurity annotation

--- src/main/java/io/adrieldg/configurations/ResourceServerConfiguration.java
+++ src/main/java/io/adrieldg/configurations/ResourceServerConfiguration.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Configuration
 @EnableResourceServer
 @RestController
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfiguration extends GlobalMethodSecurityConfiguration {
 
   @Override
--- src/main/java/io/adrieldg/configurations/WebSecurityConfiguration.java
+++ src/main/java/io/adrieldg/configurations/WebSecurityConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,7 +20,6 @@ import org.springframework.web.filter.CorsFilter;
 import io.adrieldg.services.CustomUserDetailsService;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Autowired
