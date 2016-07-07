b1387d8e84ec50199f69db3f143666330c584b02
Adriel P. de Guzman <adeguzman@stratpoint.com>
2016-07-07 16:29:21 +0800

    Modified the implementation of findRandom() to support PostgreSQL

--- src/main/java/io/adrieldg/repositories/JournalRepository.java
+++ src/main/java/io/adrieldg/repositories/JournalRepository.java
@@ -1,13 +1,13 @@
 package io.adrieldg.repositories;
 
-import java.util.Collection;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.Collection;
 
 import io.adrieldg.entities.Journal;
 
@@ -26,7 +26,8 @@ public interface JournalRepository
   Collection<Journal> findByContentsContainingIgnoreCaseOrderByPublishDateDesc(
       @Param("q") String contents);
 
-  @Query(value = "select * from journals order by rand() limit 1", nativeQuery = true)
+  @Query(value = "select * from journals offset floor(random() * (select count(*) from journals))" +
+      "LIMIT 1;", nativeQuery = true)
   Journal findRandom();
 
   @Query(value = "select volume from journals order by volume desc limit 1", nativeQuery = true)
